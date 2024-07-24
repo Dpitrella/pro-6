4c0d8a15b9fe419c9cbdd515b241381ff40b4a86
Dante Pitrella <dantepitrella1@yahoo.com>
2024-07-24 15:40:07 +0200

    ajout de works

---
+++ Backend/models/works.model.js
@@ -0,0 +1,19 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+	const Works = sequelize.define(
+		"works",
+		{
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+			},
+		imageUrl: {
+			type: DataTypes.STRING,
+			allowNull: false
+			}
+		},
+		{timestamps:false}
+	)
+	return Works
+}
